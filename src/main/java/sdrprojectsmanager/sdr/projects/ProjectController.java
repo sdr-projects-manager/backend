1b5196eed13199f58fd858818c7adf840e34b419
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-12-16 22:01:38 +0100

    style: ProjectController

--- src/main/java/sdrprojectsmanager/sdr/projects/ProjectController.java
+++ src/main/java/sdrprojectsmanager/sdr/projects/ProjectController.java
@@ -3,9 +3,6 @@ package sdrprojectsmanager.sdr.projects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sdrprojectsmanager.sdr.teams.Team;
-import sdrprojectsmanager.sdr.users.UsersRepository;
-
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -18,28 +15,28 @@ public class ProjectController {
     @Autowired
     private ProjectsRepository projectsRepository;
 
-    @RequestMapping(value = "/find/{id}",  method= RequestMethod.GET, consumes = "application/json")
+    @RequestMapping(value = "/find/{id}", method = RequestMethod.GET, consumes = "application/json")
     public ResponseEntity<?> getById(@PathVariable Integer id) {
         Optional<Project> searchResult = projectsRepository.findById(id);
         if (searchResult.isEmpty()) {
             return ResponseEntity.ok("Nie znaleziono");
-            //TODO ResponseExceptionController
+            // TODO ResponseExceptionController
         }
         return ResponseEntity.ok(searchResult);
     }
 
-    @RequestMapping(value = "/all",  method= RequestMethod.GET, consumes = "application/json")
+    @RequestMapping(value = "/all", method = RequestMethod.GET, consumes = "application/json")
     public ResponseEntity<?> getAll() {
         Iterable<Project> searchResult = projectsRepository.findAll();
         if (searchResult.equals(null)) {
             return ResponseEntity.ok("Nie znaleziono");
-            //TODO ResponseExceptionController
+            // TODO ResponseExceptionController
         }
         return ResponseEntity.ok(searchResult);
     }
 
-    @RequestMapping(value="/add", method= RequestMethod.POST, consumes = "application/json")
-    public @ResponseBody    Object add(@Valid @RequestBody Project newProject) {
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json")
+    public @ResponseBody Object add(@Valid @RequestBody Project newProject) {
         System.out.println("Create");
         Project project = new Project();
         project.setName(newProject.getName());
