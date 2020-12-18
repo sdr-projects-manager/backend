619c3ee1167c50f641d10f72fa67a84377d4c339
skwarecky <dawid.smalc@gmail.com>
2020-12-18 19:25:22 +0100

    Aadd team squad repository

--- src/main/java/sdrprojectsmanager/sdr/teams/TeamsSquadRepository.java
+++ src/main/java/sdrprojectsmanager/sdr/teams/TeamsSquadRepository.java
@@ -1,2 +1,6 @@
-package sdrprojectsmanager.sdr.teams;public interface TeamsSquadRepository {
+package sdrprojectsmanager.sdr.teams;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamsSquadRepository extends CrudRepository<TeamSquad, Integer> {
 }
