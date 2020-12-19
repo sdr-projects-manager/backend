7501771e43e93e071a0867600f1a8e5f90313748
skwarecky <dawid.smalc@gmail.com>
2020-12-19 20:03:25 +0100

    Add exception controllers

---
+++ src/main/java/sdrprojectsmanager/sdr/exception/APIException.java
@@ -0,0 +1,11 @@
+package sdrprojectsmanager.sdr.exception;
+
+public class APIException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public APIException(String message){
+        super(message);
+
+    }
+}
---
+++ src/main/java/sdrprojectsmanager/sdr/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package sdrprojectsmanager.sdr.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolationException;
+import java.util.Date;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    //Handle specific exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    //Handle Api exceptions
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<?> handleAPIException(APIException exception, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "APIException", request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    //Handle bad request exception
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleBadRequestException(ConstraintViolationException exception, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "Request exception", request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    //Handle bad request exception
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityException(EntityNotFoundException exception, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "Request exception", request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    //Handle global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception exception, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), "Internal server error", request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
