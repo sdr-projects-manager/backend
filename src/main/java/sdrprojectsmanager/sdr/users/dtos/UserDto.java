edefab9e94191bd5c34d7a0d312c448648f07fb8
skwarecky <dawid.smalc@gmail.com>
2020-12-20 13:45:14 +0100

    Update user model, repo, and controllers

--- src/main/java/sdrprojectsmanager/sdr/users/dtos/UserDto.java
+++ src/main/java/sdrprojectsmanager/sdr/users/dtos/UserDto.java
@@ -9,10 +9,29 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 public class UserDto {
+
+    @Column(nullable = false)
+    @NotNull(message = "Please provide a login")
     public String login;
+
+    @Column(nullable = false)
+    @NotNull(message = "Please provide a password")
     public String password;
+
+    @NotNull(message = "Please provide a name")
+    @Column(nullable = false)
     public String name;
+
+    @NotNull(message = "Please provide a last name")
+    @Column(nullable = false)
     public String lastName;
+
+    @NotNull(message = "Please provide a email")
+    @Column(nullable = false)
     public String email;
+
+
+    @NotNull(message = "Please provide a role id")
+    @Column(nullable = false)
     public Integer role_id;
 }
