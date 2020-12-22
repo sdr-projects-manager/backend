9250207606b3a548d864c822d91b896f087627f3
skwarecky <dawid.smalc@gmail.com>
2020-12-22 18:54:19 +0100

    Redefine team tables

--- src/main/java/sdrprojectsmanager/sdr/teams/TeamController.java
+++ src/main/java/sdrprojectsmanager/sdr/teams/TeamController.java
@@ -1,8 +1,11 @@
 package sdrprojectsmanager.sdr.teams;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sdrprojectsmanager.sdr.exception.ResourceNotFoundException;
+
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -17,45 +20,44 @@ public class TeamController {
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public ResponseEntity<?> getById(@PathVariable Integer id) {
-        Optional<Team> searchResult = teamsRepository.findById(id);
-        if (searchResult.isEmpty()) {
-            return ResponseEntity.ok("Nie znaleziono");
-            // TODO ResponseExceptionController
-        }
+        Team searchResult = teamsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Team not found with id: " + id));
         return ResponseEntity.ok(searchResult);
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> getAll() {
-        Iterable<Team> searchResult = teamsRepository.findAll();
-        if (searchResult.equals(null)) {
-            return ResponseEntity.ok("Nie znaleziono");
-            // TODO ResponseExceptionController
-        }
-        return ResponseEntity.ok(searchResult);
+        Iterable<Team> allTeams = teamsRepository.findAll();
+        if(allTeams.equals(null)) throw new ResourceNotFoundException("Teams not found");
+        return ResponseEntity.ok(allTeams);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody Object add(@Valid @RequestBody Team newTeam) {
-        System.out.println("Create");
         Team team = new Team();
-        team.setName(newTeam.getName());
-        team.setMaxPeople(newTeam.getMaxPeople());
-        teamsRepository.save(team);
+        try {
+            team.setName(newTeam.getName());
+            team.setMaxPeople(newTeam.getMaxPeople());
+            teamsRepository.save(team);
+        }
+        catch(Exception e){
+                throw new ResourceNotFoundException("Create team fails");
+        }
         return ResponseEntity.ok(team);
     }
 
-    @RequestMapping(value = "/setMaxUsers/{teamId}", method = RequestMethod.POST)
-    public @ResponseBody Object setMaxUsers(@PathVariable Integer teamId, @RequestBody Team editTeam) {
-        Team teamEdit = teamsRepository.findById(teamId).orElse(null);
-        if (teamEdit == null) {
-            return ResponseEntity.ok("Nie znaleziono");
-            // TODO ResponseExceptionController
+    @RequestMapping(value = "/edit/{teamId}", method = RequestMethod.POST)
+    public @ResponseBody Object edit(@PathVariable Integer teamId, @RequestBody Team editTeam) {
+        Team teamEdit = teamsRepository.findById(teamId)
+                .orElseThrow(() -> new ResourceNotFoundException("Team not found with id: " + teamId));
+        try{
+            teamEdit.setName(editTeam.getName());
+            teamEdit.setMaxPeople(editTeam.getMaxPeople());
+            teamsRepository.save(teamEdit);
         }
-        System.out.println(teamId);
-        System.out.println(editTeam.getMaxPeople());
-        teamEdit.setMaxPeople(editTeam.getMaxPeople());
-        teamsRepository.save(teamEdit);
+        catch(DataAccessException e){
+            throw new ResourceNotFoundException(e.getCause().getMessage());
+        }
         return ResponseEntity.ok(teamEdit);
     }
 }
