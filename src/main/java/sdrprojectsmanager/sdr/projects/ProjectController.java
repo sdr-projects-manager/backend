7368faff665a15f2cb3e044aa54905e791de4211
skwarecky <dawid.smalc@gmail.com>
2021-01-26 22:42:20 +0100

    Redefine controller for adding new project

--- src/main/java/sdrprojectsmanager/sdr/projects/ProjectController.java
+++ src/main/java/sdrprojectsmanager/sdr/projects/ProjectController.java
@@ -1,8 +1,15 @@
 package sdrprojectsmanager.sdr.projects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sdrprojectsmanager.sdr.budgets.Budget;
+import sdrprojectsmanager.sdr.budgets.BudgetsRepository;
+import sdrprojectsmanager.sdr.exception.ResourceNotFoundException;
+import sdrprojectsmanager.sdr.teams.Team;
+import sdrprojectsmanager.sdr.teams.TeamsRepository;
+
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -14,6 +21,10 @@ public class ProjectController {
 
     @Autowired
     private ProjectsRepository projectsRepository;
+    @Autowired
+    private BudgetsRepository budgetsRepository;
+    @Autowired
+    private TeamsRepository teamsRepository;
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public ResponseEntity<?> getById(@PathVariable Integer id) {
@@ -36,14 +47,30 @@ public class ProjectController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody Object add(@Valid @RequestBody Project newProject) {
-        System.out.println("Create");
+    public @ResponseBody Object add(@Valid @RequestBody AddProject newProject){
+
+        Budget newBudget = new Budget();
+        try{
+            newBudget.setLimitation(newProject.getLimitation());
+            newBudget.setCost(0.00);
+            budgetsRepository.save(newBudget);
+        }
+        catch(DataAccessException e){
+            throw new ResourceNotFoundException(e.getCause().getMessage());
+        }
+        Team searchResult = teamsRepository.findById(newProject.getTeamId())
+                .orElseThrow(() -> new ResourceNotFoundException("Team not found with id: " + newProject.getTeamId()));
         Project project = new Project();
-        project.setName(newProject.getName());
-        project.setTeamId(newProject.getTeamId());
-        project.setBudgetId(1);
-        project.setState(0);
-        projectsRepository.save(project);
+        try{
+            project.setName(newProject.getName());
+            project.setTeamId(newProject.getTeamId());
+            project.setBudget(newBudget);
+            project.setState(0);
+            projectsRepository.save(project);
+        }
+        catch(DataAccessException e){
+            throw new ResourceNotFoundException(e.getCause().getMessage());
+        }
         return ResponseEntity.ok(project);
     }
 }
