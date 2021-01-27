c99b72fdd65a6c85341ca9b1a3e15c5e86a225fa
skwarecky <dawid.smalc@gmail.com>
2021-01-27 22:50:25 +0100

    Add extra list filters

--- src/main/java/sdrprojectsmanager/sdr/tasks/TaskRepository.java
+++ src/main/java/sdrprojectsmanager/sdr/tasks/TaskRepository.java
@@ -2,6 +2,12 @@ package sdrprojectsmanager.sdr.tasks;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends CrudRepository<Task, Integer> {
+
+    List<Task> findByProjectId(Integer id);
+
+    List<Task> findByUserId(Integer userId);
 
 }
