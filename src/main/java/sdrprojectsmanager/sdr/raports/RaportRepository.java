9377614cb1b56a1fa0a05caaf73333bcb6de3af4
skwarecky <dawid.smalc@gmail.com>
2021-02-09 23:40:44 +0100

    Create cotroller and data base model

---
+++ src/main/java/sdrprojectsmanager/sdr/raports/RaportRepository.java
@@ -0,0 +1,5 @@
+package sdrprojectsmanager.sdr.raports;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RaportRepository extends CrudRepository<Raport, Integer> {}
---
+++ src/main/java/sdrprojectsmanager/sdr/raports/RaportsController.java
@@ -0,0 +1,36 @@
+package sdrprojectsmanager.sdr.raports;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sdrprojectsmanager.sdr.exception.ResourceNotFoundException;
+import sdrprojectsmanager.sdr.roles.RolesRepository;
+import sdrprojectsmanager.sdr.users.User;
+
+import javax.validation.Valid;
+
+@RestController
+@ControllerAdvice()
+@Valid
+@RequestMapping("api/raports")
+public class RaportsController {
+
+
+    @Autowired
+    private RaportRepository raportRepository;
+
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getById(@PathVariable Integer id) {
+        Raport searchResult = raportRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Raport not found with id: " + id));
+        return ResponseEntity.ok(searchResult);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody Object getAll() {
+        Iterable <Raport> allUsers = raportRepository.findAll();
+        if(allUsers.equals(null)) throw new ResourceNotFoundException("Raports not found");
+        return ResponseEntity.ok(allUsers);
+    }
+}
