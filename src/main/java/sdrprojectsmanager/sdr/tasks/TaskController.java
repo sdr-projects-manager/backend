6c6b0e79c69b033a918aaf93e6718d4589feb3fc
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-14 08:32:15 +0100

    fix: remove unused imports

--- src/main/java/sdrprojectsmanager/sdr/tasks/TaskController.java
+++ src/main/java/sdrprojectsmanager/sdr/tasks/TaskController.java
@@ -1,9 +1,7 @@
 package sdrprojectsmanager.sdr.tasks;
 
-import org.h2.util.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sdrprojectsmanager.sdr.exception.ResourceNotFoundException;
