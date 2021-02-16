39a983e33cac26d4a89baf02246898cbac74daae
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-16 21:15:22 +0100

    fix: retrieve current user from repository

--- src/main/java/sdrprojectsmanager/sdr/currentUser/CurrentUserController.java
+++ src/main/java/sdrprojectsmanager/sdr/currentUser/CurrentUserController.java
@@ -1,14 +1,15 @@
 package sdrprojectsmanager.sdr.currentUser;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
+import sdrprojectsmanager.sdr.users.User;
+import sdrprojectsmanager.sdr.users.UsersRepository;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -16,8 +17,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CurrentUserController {
 
+  private final UsersRepository userRepository;
+
   @RequestMapping(method = RequestMethod.GET)
-  public @ResponseBody Object getMe() {
-    return ResponseEntity.ok(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+  public @ResponseBody Object getMe(Authentication authentication) {
+    User user = userRepository.findByLoginIgnoreCase(authentication.getName());
+    return ResponseEntity.ok(user);
   }
 }
